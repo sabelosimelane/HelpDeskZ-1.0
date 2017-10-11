aff6c6036cbd3905f96b12ded58aabd232d95a94
Sabelo Simelane <sabside@gmail.com>
2017-10-11 19:38:23 +0200

    Moved Submit ticket inside login

---
+++ includes/config.php
@@ -0,0 +1,8 @@
+<?php
+	$config['Database']['dbname'] = 'helpdeskz';
+	$config['Database']['tableprefix'] = 'hdzie_';
+	$config['Database']['servername'] = 'localhost';
+	$config['Database']['username'] = 'helpdeskz';
+	$config['Database']['password'] = 'Musn12nat';
+	$config['Database']['type'] = 'mysqli';
+	?>
